94df8056c9f7cf91b5bd433927603025d4a846f9
Farid Ahamat <farid.ahamat@gmail.com>
2022-07-18 13:53:51 +0800

    Added Keycloak auth
    Added Keycloak auth. Keycloak configuration is done separately

--- DemoNothing/Program.cs
+++ DemoNothing/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +7,20 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(o =>
+    {
+        o.Authority = "http://localhost:8080/auth/realms/demo-nothing";
+        o.Audience = "demo-client";
+    })
+    .AddKeycloak(o =>
+    {
+        o.ClientId = "demo-client";
+        o.TokenEndpoint = "http://localhost:8080/auth";
+        o.AccessType = AspNet.Security.OAuth.Keycloak.KeycloakAuthenticationAccessType.Public;
+    });
+
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
@@ -16,8 +32,17 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+// Should be configured based on what we want to allow
+app.UseCors(o =>
+{
+    o.AllowAnyOrigin();
+    o.AllowAnyHeader();
+    o.AllowAnyMethod();
+});
+
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
